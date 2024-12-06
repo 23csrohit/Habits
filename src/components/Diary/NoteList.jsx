9d53703e1821a66de5779ef37f101e976b7c3973
23csrohit <149533311+23csrohit@users.noreply.github.com>
2024-12-06 22:33:54 +0530

    first commit

---
+++ src/components/Diary/NoteList.jsx
@@ -0,0 +1,34 @@
+import styles from '../../css/NoteList.module.css';
+
+// framer
+import { AnimatePresence } from 'framer-motion';
+
+// components
+import Note from './Note';
+
+function NoteList({ diary, onStartEditNote, onDeleteNote }) {
+	const notes = [...diary]
+		.reverse()
+		.map(
+			(note) => (
+				<Note
+					key={note.date}
+					text={note.text}
+					date={note.date}
+					{...{ onStartEditNote, onDeleteNote }}
+				/>
+			)
+		);
+
+	return (
+		<div>
+			<ul className={styles.list}>
+				<AnimatePresence initial={false}>
+					{notes}
+				</AnimatePresence>
+			</ul>
+		</div>
+	);
+}
+
+export default NoteList;
